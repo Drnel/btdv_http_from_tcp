eac359bdd0df3bbd6742c820e9c827c844136f4b
Drnel <mahesh121995@gmail.com>
2025-06-29 21:55:46 +0000

    made main read and print bytes

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
-
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Printf("File open error: %s", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	file_read_buffer := make([]byte, 8)
+	for {
+		_, err := file.Read(file_read_buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Printf("File read error: %s", err)
+				os.Exit(1)
+			}
+		}
+		fmt.Printf("read: %s\n", file_read_buffer)
+	}
 }
