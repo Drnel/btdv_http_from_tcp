86b7922eddfdaf8247c5cda476f8607e05540eed
Drnel <mahesh121995@gmail.com>
2025-07-06 22:19:30 +0000

    changed parsing to read chunks instead of entire stream

--- internal/request/request.go
+++ internal/request/request.go
@@ -6,8 +6,11 @@ import (
 	"strings"
 )
 
+const bufferSize = 8
+
 type Request struct {
 	RequestLine RequestLine
+	parserState int // 0 initialized | 1 done
 }
 
 type RequestLine struct {
@@ -17,35 +20,74 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	request_string, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+	buf := make([]byte, bufferSize, bufferSize)
+	readToIndex := 0
+	request := Request{}
+	request.parserState = 0
+	for request.parserState != 1 {
+		if readToIndex == len(buf) {
+			new_buf := make([]byte, len(buf)*2, len(buf)*2)
+			copy(new_buf, buf)
+			buf = new_buf
+		}
+		n, err := reader.Read(buf[readToIndex:])
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				request.parserState = 1
+				break
+			}
+			return &request, err
+		}
+		readToIndex += n
+		bytes_read, err := request.parse(buf[:readToIndex])
+		if err != nil {
+			return &request, err
+		}
+		copy(buf, buf[bytes_read:])
+		readToIndex = readToIndex - bytes_read
 	}
-	request_lines := strings.Split(string(request_string), "\r\n")
-	request_line, err := parseRequest(request_lines[0])
-	if err != nil {
-		return nil, err
-	}
-	request := Request{request_line}
 	return &request, nil
 }
 
-func parseRequest(request_line string) (RequestLine, error) {
-	parts := strings.Split(request_line, " ")
+func parseRequestLine(request_string string) (RequestLine, int, error) {
+	request_lines := strings.Split(request_string, "\r\n")
+	if len(request_lines) == 1 {
+		return RequestLine{}, 0, nil
+	}
+	parts := strings.Split(request_lines[0], " ")
 	if len(parts) != 3 {
-		return RequestLine{}, errors.New("Error parsing request line")
+		return RequestLine{}, 0, errors.New("Error parsing request line")
 	}
 	rl := RequestLine{}
 	rl.Method = parts[0]
 	for _, char := range rl.Method {
 		if char < 'A' || char > 'Z' {
-			return RequestLine{}, errors.New("Got invalid method")
+			return RequestLine{}, 0, errors.New("Got invalid method")
 		}
 	}
 	rl.RequestTarget = parts[1]
 	if parts[2] != "HTTP/1.1" {
-		return RequestLine{}, errors.New("Invalid http version")
+		return RequestLine{}, 0, errors.New("Invalid http version")
 	}
 	rl.HttpVersion = "1.1"
-	return rl, nil
+	return rl, len(request_lines[0]), nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	if r.parserState == 0 {
+		RequestLine, bytes_read, err := parseRequestLine(string(data))
+		if err != nil {
+			return 0, err
+		}
+		if bytes_read == 0 {
+			return 0, nil
+		}
+		r.RequestLine = RequestLine
+		r.parserState = 1
+		return bytes_read, nil
+	}
+	if r.parserState == 1 {
+		return 0, errors.New("error: trying to read data in a done state")
+	}
+	return 0, errors.New("error: unknown state")
 }
